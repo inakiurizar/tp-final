6fd28ee2f161e442cd12f1bc6e30162598f3e338
inakiurizar <101410794+inakiurizar@users.noreply.github.com>
2022-06-06 23:01:09 -0300

    update 2.0 - 5/6

---
+++ Assets/Scripts/playerMovementMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovementMouse : MonoBehaviour
+{
+    public float sensibilidad = 100f;
+    public Transform player;
+    float xRotation = 0f;
+    float mouseX, mouseY;
+    float yRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X")*sensibilidad;
+        mouseY += Input.GetAxis("Mouse Y")*sensibilidad;
+
+        xRotation -= mouseY;
+        yRotation += mouseX;
+        xRotation -= Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.Rotate(Vector3.up * yRotation);
+    }
+}
